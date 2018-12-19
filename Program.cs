7fe4c0ef8af3861c3a15446b39995175dd250d8e
MickaelHm <harmeau.mickael@gmail.com>
2018-12-19 15:33:49 +0100

    Complet!

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rdm = new Random();
+            int randomNumber = rdm.Next(1, 51);
+            Console.WriteLine("Dévinez le nombre que j'ai choisi entre 1 et 50");
+            int userNumber = int.Parse(Console.ReadLine());
+            int loop = 0;
+            while (userNumber != randomNumber)
+            {
+                if (userNumber > randomNumber)
+                {
+                    Console.WriteLine("Le nombre à deviner est plus petit, essaie encore!");
+                }
+                else
+                {
+                    Console.WriteLine("Le nombre à deviner est plus grand, essaie encore!");
+                }
+                loop++;
+                userNumber = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Bravo, vous avez trouvé! \n Nombre de tentative:" + loop);
+        }
+    }
+}
