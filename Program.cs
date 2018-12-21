110f85de03e4fb08c4de91a145709441791f682c
MickaelHm <harmeau.mickael@gmail.com>
2018-12-21 12:43:56 +0100

    correction

--- Program.cs
+++ Program.cs
@@ -10,19 +10,29 @@ namespace TP2
     {
         static void Main(string[] args)
         {
-            Random rdm = new Random();
-            int randomNumber = rdm.Next(1, 51);
+            Random rdm = new Random();// déclaration d'une variable rdm qui va générer un chiffre aléatoir
+            int randomNumber = rdm.Next(1, 51); // déclaration d'une variable a laquelle on affecte le chiffre aléatoire choisi par le code et défini dans la fourchette choisie. Sachant qu'avec la fonction next le "51" est exclu de la fourchette
+            Console.ForegroundColor = ConsoleColor.Red;//rajout de couleur bonus proposé par Amélie, couleur texte
+            Console.BackgroundColor = ConsoleColor.Green; // changement de couleur de fond
+            Console.SetCursorPosition(10, 1); // changement de position du texte (x, y)
             Console.WriteLine("Dévinez le nombre que j'ai choisi entre 1 et 50");
             int userNumber = int.Parse(Console.ReadLine());
             int loop = 0;
             while (userNumber != randomNumber)
             {
-                if (userNumber > randomNumber)
+                if (userNumber <1 || userNumber >50)
                 {
+                    //Console.Clear(); permet d'effacer la console
+                    Console.WriteLine("Oops, votre nombre n'est pas compris entre 1 et 50!");
+                }
+                else if (userNumber > randomNumber)
+                {
+                    //Console.Clear(); permet d'effacer la console
                     Console.WriteLine("Le nombre à deviner est plus petit, essaie encore!");
                 }
                 else
                 {
+                    //Console.Clear(); permet d'effacer la console
                     Console.WriteLine("Le nombre à deviner est plus grand, essaie encore!");
                 }
                 loop++;
